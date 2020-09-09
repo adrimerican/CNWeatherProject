41caac438451731c08d5c7ae908e6bb43e511d99
Hamza <hamzam1478@gmail.com>
2020-09-09 15:43:04 +0100

    Working WeatherApp

---
+++ hamza/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const hbs = require("express-handlebars");
+const path = require("path");
+const getWeather = require("./lib/getWeather.js");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.engine(
+  "hbs",
+  hbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+    layoutsDir: path.join(__dirname, "views", "layouts"),
+    partialsDir: path.join(__dirname, "views", "partials"),
+  })
+);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", ".hbs");
+
+app.get("/", async (req, res) => {
+  let data = await getWeather();
+  let name = data.name;
+  let description = data.weather[0].description;
+  let temp = data.main.temp;
+  let feels_like = data.main.feels_like;
+  res.render("index", { name, temp, data: { description, feels_like } });
+});
+app.get("/weather", (req, res) => {
+  res.render("weather");
+});
+app.get("*", (req, res) => {
+  res.render("404");
+});
+
+app.listen(3000, () => {
+  console.log("listening to port 3000");
+});
