76e3c188df67e47cdbe3f3835c7967533410c7c3
Hamza <hamzam1478@gmail.com>
2020-09-10 10:12:42 +0100

    Requested changes made.

--- hamza/lib/getWeather.js
+++ hamza/lib/getWeather.js
@@ -1,11 +1,10 @@
 const fetch = require("node-fetch");
-const APPID = "2c62c24727abdfa20dff48cf1e1e009b";
 require("dotenv").config();
 
 const url = `http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&units=metric&appid=${process.env.APPID}`;
 
 const getWeather = async () => {
-  let data = await fetch(url);
+  const data = await fetch(url);
   return await data.json();
 };
 
